f4010fb64950bade6b2681aaf2057acf8f89c07a
diana <dianagiosan@yahoo.com>
2020-07-22 17:15:46 +0300

    Separated the WithdrawalController, HomePageController, BalanceCheckController and ATMRefillController for better code management.

--- src/main/java/com/example/ATMProject/WithdrawalController.java
+++ src/main/java/com/example/ATMProject/WithdrawalController.java
@@ -2,18 +2,16 @@ package com.example.ATMProject;
 
 import com.example.ATMProject.client.ATMClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-public class ATMController {
+public class WithdrawalController {
 	@Autowired
 	ATMService ATMinstance;
 	@Autowired
 	ATMClient myClient;
 	
-	@GetMapping("/new-transaction")
+	@GetMapping("/api/new-transaction")
 	public String transaction(@RequestParam(value = "sum", defaultValue = "0") int cashToWithdraw) {
 		if (ATMinstance.splitIntoBills(cashToWithdraw).message.equals("Transaction approved"))
 			return ATMinstance.splitIntoBills(cashToWithdraw).toString();

c0b362a68b3a8d2794532724da5130934209e0da
diana <dianagiosan@yahoo.com>
2020-07-22 17:42:05 +0300

    Code cleanup.

--- src/main/java/com/example/ATMProject/WithdrawalController.java
+++ src/main/java/com/example/ATMProject/WithdrawalController.java
@@ -2,7 +2,9 @@ package com.example.ATMProject;
 
 import com.example.ATMProject.client.ATMClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WithdrawalController {
@@ -13,9 +15,11 @@ public class WithdrawalController {
 	
 	@GetMapping("/api/new-transaction")
 	public String transaction(@RequestParam(value = "sum", defaultValue = "0") int cashToWithdraw) {
+		/* Attempt to withdraw cash from this ATM */
 		ATMOutput attemptOutput = ATMinstance.splitIntoBills(cashToWithdraw);
 		if (attemptOutput.message.equals("Transaction approved"))
 			return attemptOutput.toString();
+			/* If not possible, try to withdraw from the other available ATM */
 		else return myClient.transaction(cashToWithdraw);
 	}
 	

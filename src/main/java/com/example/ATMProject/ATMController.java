892a602f6a17d4467df69efbb94cad027c9d97e7
diana <dianagiosan@yahoo.com>
2020-07-22 11:56:45 +0300

    Added OpenFeign Client integration, so that when the ATM runs out of cash, it can draw cash from another remote ATM.

--- src/main/java/com/example/ATMProject/ATMController.java
+++ src/main/java/com/example/ATMProject/ATMController.java
@@ -1,5 +1,6 @@
 package com.example.ATMProject;
 
+import com.example.ATMProject.client.ATMClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,12 +10,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class ATMController {
 	@Autowired
 	ATMService ATMinstance;
+	@Autowired
+	ATMClient myClient;
+	
 	@GetMapping("/new-transaction")
-	public ATMOutput transaction(@RequestParam(value = "sum", defaultValue = "0") int cashToWithdraw){
-		return ATMinstance.splitIntoBills(cashToWithdraw);
+	public String transaction(@RequestParam(value = "sum", defaultValue = "0") int cashToWithdraw) {
+		if (ATMinstance.splitIntoBills(cashToWithdraw).message.equals("Transaction approved"))
+			return ATMinstance.splitIntoBills(cashToWithdraw).toString();
+		else return myClient.transaction(cashToWithdraw);
 	}
+	
 	@GetMapping("/")
-	public String HelloMessage(){
+	public String HelloMessage() {
 		return "Hello! Transfer some money.";
 		
 	}

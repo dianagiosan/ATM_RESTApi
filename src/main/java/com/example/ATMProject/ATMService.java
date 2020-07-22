8061b008e14c06282dabd9dfb7a7a26196692be2
diana <dianagiosan@yahoo.com>
2020-07-22 17:15:01 +0300

    Separated the WithdrawalController, HomePageController, BalanceCheckController and ATMRefillController for better code management.

--- src/main/java/com/example/ATMProject/ATMService.java
+++ src/main/java/com/example/ATMProject/ATMService.java
@@ -1,10 +1,13 @@
 package com.example.ATMProject;
 
+import java.util.List;
+
 /**
  * The ATMService base interface
  */
 public interface ATMService {
-	
+	void ATMRefill(int billValue, int billAmount);
 	ATMOutput splitIntoBills(int cashToWithdraw);
 	
+	List<billEntry> getAvailableBills();
 }

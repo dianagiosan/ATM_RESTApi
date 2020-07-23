8ab0c246513f3e0cc1a5d9bff27ae779d3be5df7
diana <dianagiosan@yahoo.com>
2020-07-23 17:15:00 +0300

    Added two integration tests with Rest Assured.

--- src/test/java/com/example/ATMProject/IntegrationTests.java
+++ src/test/java/com/example/ATMProject/IntegrationTests.java
@@ -1,31 +1,36 @@
 package com.example.ATMProject;
 
-import io.restassured.RestAssured;
-import io.restassured.response.Response;
-import io.restassured.response.ResponseBody;
-import io.restassured.specification.RequestSpecification;
 import org.json.JSONException;
-import org.json.JSONObject;
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Map;
 import java.util.TreeMap;
 
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.*;
+import static io.restassured.RestAssured.get;
 
 public class IntegrationTests {
 	@Test
-	public void withdraw101Test() throws JSONException {
-		RestAssured.baseURI = "http://localhost:8080";
-		RequestSpecification request = RestAssured.given();
-		JSONObject requestParams = new JSONObject();
-		requestParams.put("sum", "101");
-		Response response = request.get("/api/new-transaction");
-		ResponseBody body = response.getBody();
-		ATMOutput ATMbody = body.as(ATMOutput.class);
-		Assert.assertEquals("Transaction approved", ATMbody.message);
-		
+	public void withdraw101Test() {
+		ATMOutput output = get("http://localhost:8080/api/new-transaction?sum=101").as(ATMOutput.class);
+		Map <String, Integer> expected = new TreeMap<>();
+		expected.put("ONEHUNDRED_RON(100)", 1);
+		expected.put("ONE_RON(1)", 1);
+		Assert.assertEquals(output.bills, expected);
+		Assert.assertEquals(output.message, "Transaction approved");
+	}
+	@Test
+	public void withdraw101and1000Test() {
+		ATMOutput output = get("http://localhost:8080/api/new-transaction?sum=101").as(ATMOutput.class);
+		Map <String, Integer> expected = new TreeMap<>();
+		expected.put("ONEHUNDRED_RON(100)", 1);
+		expected.put("ONE_RON(1)", 1);
+		Assert.assertEquals(output.bills, expected);
+		Assert.assertEquals(output.message, "Transaction approved");
+		output = get("http://localhost:8080/api/new-transaction?sum=1000").as(ATMOutput.class);
+		expected.clear();
+		expected.put("ONEHUNDRED_RON(100)", 10);
+		Assert.assertEquals(output.bills, expected);
+		Assert.assertEquals(output.message, "Transaction approved");
 	}
 }

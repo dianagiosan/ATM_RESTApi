9f1cc191856b324210964c546f8e64e5f304ac93
diana <dianagiosan@yahoo.com>
2020-07-23 15:40:20 +0300

    Added swagger-2 integration, as well as Integration tests with Rest Assured.

--- src/main/java/com/example/ATMProject/BillEntry.java
+++ src/main/java/com/example/ATMProject/BillEntry.java
@@ -7,6 +7,7 @@ import java.util.Objects;
  * the amount of such bills that are available in the ATM
  */
 public class BillEntry {
+	
 	private int billValue;
 	private int billAmount;
 	
@@ -33,6 +34,20 @@ public class BillEntry {
 	
 	public String toString() {
 		return "value: " + billValue + " -> amount: " + billAmount + " ";
+	}
+	
+	public String billValueAsString() {
+		if(billValue == 1)
+			return "ONE_RON(1)";
+		else if(billValue == 5)
+			return "FIVE_RON(5)";
+		else if(billValue == 10)
+			return "TEN_RON(10)";
+		else if(billValue == 50)
+			return "FIFTY_RON(50)";
+		else if(billValue == 100)
+			return "ONEHUNDRED_RON(100)";
+		return "";
 	}
 	
 	@Override

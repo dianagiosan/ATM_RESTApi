971c57b4d4c2969d2a672d4a82f3b9bf02a222db
diana <dianagiosan@yahoo.com>
2020-08-11 12:32:02 +0300

    Last tag I swear

--- src/main/java/com/example/ATMProject/Application/Service/MailService.java
+++ src/main/java/com/example/ATMProject/Application/Service/MailService.java
@@ -3,5 +3,7 @@ package com.example.ATMProject.Application.Service;
 import net.sf.jasperreports.engine.JRException;
 
 public interface MailService {
+	
+	
 	 void send(String mailContents) throws JRException;
 }
